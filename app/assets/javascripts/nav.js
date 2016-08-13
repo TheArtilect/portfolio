f5e09c04efddfbee0c817d579093d3b08020d399
Ian Agpawa <agpawaji@gmail.com>
2016-08-13 20:41:48 +0000

    nav bar styling

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,3 +1,4 @@
+var page = "resume";
 
 function navIt(){
   var shown = false;
@@ -10,5 +11,28 @@ function navIt(){
       shown = true;
     }
   })
+  
+
+  function colorNav(){
+    if (page == "home"){
+      $(".n-link").css("color", "white")
+    } else if (page == "resume"){
+      $(".n-link").css("color", "black")
+    }
+  }
+  
+  $("#page-link").on("click", function(){
+    page = "home";
+
+  })
+  
+  $("#resume-link").on("click", function(){
+    page = 'resume';
+
+  })
+  
+  colorNav();
 }
+
+
 
